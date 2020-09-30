db5c251f45bafec4e0d8b0da9c2935bc3af282a6
yy1363534009@163.com <yy1363534009@163.com>
2020-10-01 00:57:31 +0800

    十月一凌晨一点：样式调整

--- pages/jobsearch/jobsearch.js
+++ pages/jobsearch/jobsearch.js
@@ -2,6 +2,35 @@ const app = getApp();
 Page({
   data: {
     CustomBar: app.globalData.CustomBar,
+    swiperList: [{
+      id: 0,
+      type: 'image',
+      url: 'https://ossweb-img.qq.com/images/lol/web201310/skin/big84000.jpg'
+    }, {
+      id: 1,
+        type: 'image',
+        url: 'https://ossweb-img.qq.com/images/lol/web201310/skin/big84001.jpg',
+    }, {
+      id: 2,
+      type: 'image',
+      url: 'https://ossweb-img.qq.com/images/lol/web201310/skin/big39000.jpg'
+    }, {
+      id: 3,
+      type: 'image',
+      url: 'https://ossweb-img.qq.com/images/lol/web201310/skin/big10001.jpg'
+    }, {
+      id: 4,
+      type: 'image',
+      url: 'https://ossweb-img.qq.com/images/lol/web201310/skin/big25011.jpg'
+    }, {
+      id: 5,
+      type: 'image',
+      url: 'https://ossweb-img.qq.com/images/lol/web201310/skin/big21016.jpg'
+    }, {
+      id: 6,
+      type: 'image',
+      url: 'https://ossweb-img.qq.com/images/lol/web201310/skin/big99008.jpg'
+    }],
     checkbox: [{
       value: 0,
       name: '10元',
@@ -56,5 +85,8 @@ Page({
     this.setData({
       checkbox: items
     })
+  },
+  search(event) {
+    console.log('搜索的值是：'+event.detail.value);
   }
 })
--- pages/order/order.js
+++ pages/order/order.js
@@ -8,9 +8,37 @@ Page({
     // 导航start
     TabCur: 0,
     scrollLeft:0,
-    tab:['全部订单','待付款','待开工','待确认','待评价','退款 / 售后']
-
+    tab:['全部订单','待付款','待开工','待确认','待评价','退款 / 售后'],
     // 导航end
+    swiperList: [{
+      id: 0,
+      type: 'image',
+      url: 'https://ossweb-img.qq.com/images/lol/web201310/skin/big84000.jpg'
+    }, {
+      id: 1,
+        type: 'image',
+        url: 'https://ossweb-img.qq.com/images/lol/web201310/skin/big84001.jpg',
+    }, {
+      id: 2,
+      type: 'image',
+      url: 'https://ossweb-img.qq.com/images/lol/web201310/skin/big39000.jpg'
+    }, {
+      id: 3,
+      type: 'image',
+      url: 'https://ossweb-img.qq.com/images/lol/web201310/skin/big10001.jpg'
+    }, {
+      id: 4,
+      type: 'image',
+      url: 'https://ossweb-img.qq.com/images/lol/web201310/skin/big25011.jpg'
+    }, {
+      id: 5,
+      type: 'image',
+      url: 'https://ossweb-img.qq.com/images/lol/web201310/skin/big21016.jpg'
+    }, {
+      id: 6,
+      type: 'image',
+      url: 'https://ossweb-img.qq.com/images/lol/web201310/skin/big99008.jpg'
+    }]
   },
 
   /**

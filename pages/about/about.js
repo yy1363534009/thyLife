4b0534e25b79120a8dfd71ab1e084d19b48db962
yy1363534009@163.com <yy1363534009@163.com>
2020-10-08 01:19:15 +0800

    20201008 01:19

--- pages/about/about.js
+++ pages/about/about.js
@@ -3,9 +3,9 @@ const app = getApp()
 
 Page({
   data: {
-    //-------------
-    StatusBar: app.globalData.StatusBar,
-    CustomBar: app.globalData.CustomBar,
+    userInfo: {},
+    hasUserInfo: false,
+    canIUse: wx.canIUse('button.open-type.getUserInfo'),
     iconList: [{
       icon: 'cardboardfill',
       color: 'red',
@@ -57,30 +57,73 @@ Page({
       badge: 0,
       name: '版权'
     }],
-    gridCol:3,
-    skin: false
+    gridCol: 3
   },
   onLoad: function () {
+    this.onLoadUserInfo();
   },
-//----------
-showModal(e) {
-  this.setData({
-    modalName: e.currentTarget.dataset.target
-  })
-},
-hideModal(e) {
-  this.setData({
-    modalName: null
-  })
-},
-gridchange: function (e) {
-  this.setData({
-    gridCol: e.detail.value
-  });
-},
-gridswitch: function (e) {
-  this.setData({
-    gridBorder: e.detail.value
-  });
-}
+  /**
+   * 页面加载获取用户信息
+   */
+  onLoadUserInfo() {
+    if (app.globalData.userInfo) {
+      this.setData({
+        userInfo: app.globalData.userInfo,
+        hasUserInfo: true
+      })
+    } else if (this.data.canIUse) {
+      // 由于 getUserInfo 是网络请求，可能会在 Page.onLoad 之后才返回
+      // 所以此处加入 callback 以防止这种情况
+      app.userInfoReadyCallback = res => {
+        this.setData({
+          userInfo: res.userInfo,
+          hasUserInfo: true
+        })
+      }
+    } else {
+      // 在没有 open-type=getUserInfo 版本的兼容处理
+      wx.getUserInfo({
+        success: res => {
+          app.globalData.userInfo = res.userInfo
+          this.setData({
+            userInfo: res.userInfo,
+            hasUserInfo: true
+          })
+        }
+      })
+    }
+  },
+  /**
+   * 获取用户信息
+   */
+  getUserInfo: function(e) {
+    console.log(e)
+    app.globalData.userInfo = e.detail.userInfo
+    this.setData({
+      userInfo: e.detail.userInfo,
+      hasUserInfo: true
+    })
+  },
+  //----菜单start------
+  showModal(e) {
+    this.setData({
+      modalName: e.currentTarget.dataset.target
+    })
+  },
+  hideModal(e) {
+    this.setData({
+      modalName: null
+    })
+  },
+  gridchange: function (e) {
+    this.setData({
+      gridCol: e.detail.value
+    });
+  },
+  gridswitch: function (e) {
+    this.setData({
+      gridBorder: e.detail.value
+    });
+  }
+  //----菜单end------
 })
